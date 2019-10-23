06efc9f565fffb8160fb76b34685af688265171c
dba98 <dg.borges1@gmail.com>
2019-10-23 18:49:44 +0100

    First Commit
    Criação do Projeto

---
+++ src/main/java/com/plataforma/explicacoes/ExplicacoesApplication.java
@@ -0,0 +1,13 @@
+package com.plataforma.explicacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplicacoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExplicacoesApplication.class, args);
+    }
+
+}
