11c0c821375ea3e28866dac0cde674933be5fad1
CatarinaSilva98 <35367@ufp.edu.pt>
2019-11-12 17:35:23 +0000

    Criação de curso

---
+++ src/main/java/com/plataforma/explicacoes/models/Curso.java
@@ -0,0 +1,32 @@
+package com.plataforma.explicacoes.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private Integer codigo;
+
+    /*@ManyToOne
+    private Faculdade faculdade;*/
+
+    @OneToMany
+    private Set<Cadeira> cadeira= new HashSet<>();
+    @OneToMany
+    private Set<Aluno> aluno= new HashSet<>();
+
+    public Curso(String nome, Integer codigo) {
+        this.nome = nome;
+        this.codigo = codigo;
+    }
+}
