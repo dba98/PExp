2efcd2a625da06a433b9221f5fa5b994b249e9d8
CatarinaSilva98 <35367@ufp.edu.pt>
2019-12-09 19:58:56 +0000

    Create FaculdadeController.java

--- src/main/java/com/plataforma/explicacoes/repositories/FaculdadeRepo.java
+++ src/main/java/com/plataforma/explicacoes/repositories/FaculdadeRepo.java
@@ -4,6 +4,9 @@ import com.plataforma.explicacoes.models.Faculdade;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FaculdadeRepo extends CrudRepository<Faculdade,Long> {
+    Optional<Faculdade> findByName(String name);
 }
--- src/main/java/com/plataforma/explicacoes/services/FaculdadeService.java
+++ src/main/java/com/plataforma/explicacoes/services/FaculdadeService.java
@@ -2,6 +2,8 @@ package com.plataforma.explicacoes.services;
 
 import com.plataforma.explicacoes.models.Faculdade;
 import com.plataforma.explicacoes.repositories.FaculdadeRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,8 @@ public class FaculdadeService {
 
     @Autowired
     private FaculdadeRepo faculdadeRepo;
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public Set<Faculdade> findAll(){
         Set<Faculdade> faculdades = new HashSet<>();
@@ -31,6 +35,8 @@ public class FaculdadeService {
     }
 
     public Optional<Faculdade> createFaculdade(Faculdade faculdade){
+        this.logger.info("Estou aqui");
+        this.logger.info(faculdade.toString());
         Optional<Faculdade> optionalFaculdade = this.faculdadeRepo.findByName(faculdade.getName());
         if(optionalFaculdade.isEmpty())
             return  Optional.of(this.faculdadeRepo.save(faculdade));
