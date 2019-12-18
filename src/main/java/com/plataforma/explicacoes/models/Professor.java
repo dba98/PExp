c5a509b671b8a7a76644bab3f40a906c3bd44b8e
CatarinaSilva98 <35367@ufp.edu.pt>
2019-12-18 21:05:16 +0000

    Revert "Update Professor.java"
    This reverts commit 3d2f3f025cb6ef65d48f0164b7536c730b0f2ca1.

--- src/main/java/com/plataforma/explicacoes/models/Professor.java
+++ src/main/java/com/plataforma/explicacoes/models/Professor.java
@@ -62,19 +62,14 @@ public class Professor {
         this.setId(id);
         this.setName(name);
         this.setNum(num);
-        this.associateQualificacao(grau);
-        for(Atendimento atendimento: atendimentos)
-            this.addAtendimento(atendimento);
-        for(Cadeira cadeira: cadeiras)
-            this.addCadeira(cadeira);
-        for(Idioma idioma: idiomas)
-            this.addIdioma(idioma);
-        for(Horario horario:horarios)
+        this.setGrau(grau);
+        this.setAtendimentos(atendimentos);
+        this.setCadeiras(cadeiras);
+        this.setIdiomas(idiomas);
+//        this.setHorarios(horarios);
+        for(Horario horario:horarios){
             this.addHorario(horario);
-    }
-
-    public void addAtendimento(Atendimento atendimento){
-        this.atendimentos.add(atendimento);
+        }
     }
 
     public void addCadeira(Cadeira cadeira) {
