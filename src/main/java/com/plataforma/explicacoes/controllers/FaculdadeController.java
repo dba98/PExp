c50f6a6a73049e1ac562972b718a1b7146b917d8
CatarinaSilva98 <35367@ufp.edu.pt>
2020-01-10 16:09:22 +0000

    Revert "Create FaculdadeAlreadyExistsException.java"
    This reverts commit c57cab3946ee3e914d2ac0ec062beb8355ce96a2.

--- src/main/java/com/plataforma/explicacoes/controllers/FaculdadeController.java
+++ src/main/java/com/plataforma/explicacoes/controllers/FaculdadeController.java
@@ -1,12 +1,12 @@
 package com.plataforma.explicacoes.controllers;
 
-import com.plataforma.explicacoes.exceptions.FaculdadeAlreadyExistsException;
-import com.plataforma.explicacoes.exceptions.NoFaculdadeException;
+
 import com.plataforma.explicacoes.models.Faculdade;
 import com.plataforma.explicacoes.services.FaculdadeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -24,7 +24,7 @@ public class FaculdadeController {
     private FaculdadeService faculdadeService;
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Faculdade> createFaculdade (@RequestBody Faculdade faculdade) throws FaculdadeAlreadyExistsException{
+    public ResponseEntity<Faculdade> createFaculdade (@RequestBody Faculdade faculdade){
 
         this.logger.info(faculdade.toString());
         Optional<Faculdade> optionalFaculdade = this.faculdadeService.createFaculdade(faculdade);
@@ -40,7 +40,7 @@ public class FaculdadeController {
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public  ResponseEntity<Faculdade> getFaculdadeById (@PathVariable("id") Long id) throws NoFaculdadeException{
+    public  ResponseEntity<Faculdade> getFaculdadeById (@PathVariable("id") Long id) throws FaculdadeController.NoFaculdadeException{
         this.logger.info("Received a get request");
 
         Optional<Faculdade> optionalCadeira = this.faculdadeService.findById(id);
@@ -50,7 +50,7 @@ public class FaculdadeController {
     }
 
     @RequestMapping(value = "/name/{name}", method = RequestMethod.GET)
-    public  ResponseEntity<Faculdade> getFaculdadeByName (@PathVariable("name") String name) throws NoFaculdadeException {
+    public  ResponseEntity<Faculdade> getFaculdadeByName (@PathVariable("name") String name) throws FaculdadeController.NoFaculdadeException {
         this.logger.info("Received a get request");
 
         Optional<Faculdade> optionalFaculdade = this.faculdadeService.findByName(name);
@@ -58,4 +58,25 @@ public class FaculdadeController {
             return ResponseEntity.ok(optionalFaculdade.get());
         throw new NoFaculdadeException(name);
     }
+
+    // -------------  Exception --------------------
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Faculdade already exits")
+    private static class FaculdadeAlreadyExistsException extends RuntimeException {
+        private FaculdadeAlreadyExistsException(String name) {
+            super("A faculdade with name: "+name+"already exists");
+        }
+    }
+
+    @ResponseStatus( value = HttpStatus.NOT_FOUND, reason = "No such Faculdade")
+    private static class NoFaculdadeException extends RuntimeException {
+
+        private NoFaculdadeException(Long id) {
+            super("No such Faculdade with id: "+id);
+        }
+
+        private NoFaculdadeException(String name) {
+            super("No such Faculdade with name: "+name);
+        }
+    }
+
 }
--- src/main/java/com/plataforma/explicacoes/exceptions/FaculdadeAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.plataforma.explicacoes.exceptions;
-
-public class FaculdadeAlreadyExistsException extends Throwable {
-    public FaculdadeAlreadyExistsException(String name){
-        super("Faculdade "+name+" already exists");
-    }
-}
