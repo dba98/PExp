f70811aa6eab67c3b7881be5861778b6b553de38
dba98 <dg.borges1@gmail.com>
2020-01-12 18:43:42 +0000

    Ligação dos filtros com o WS2 e acréscimo de dois novos filtros

--- src/main/java/com/plataforma/explicacoes/services/filters/professor/FilterProfessorObject.java
+++ src/main/java/com/plataforma/explicacoes/services/filters/professor/FilterProfessorObject.java
@@ -1,5 +1,7 @@
 package com.plataforma.explicacoes.services.filters.professor;
 
+import com.plataforma.explicacoes.models.Idioma;
+import com.plataforma.explicacoes.models.Qualificacao;
 import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,12 +17,16 @@ public class FilterProfessorObject {
     private DayOfWeek dia;
     private LocalTime inicio;
     private LocalTime fim;
-
-    public FilterProfessorObject(Integer curso, DayOfWeek dia, LocalTime inicio, LocalTime fim) {
+    private String idioma;
+    private Integer grau;
+    public FilterProfessorObject(Integer curso, DayOfWeek dia, LocalTime inicio, LocalTime fim, String idioma, Integer grau) {
         this.curso = curso;
         this.dia = dia;
         this.inicio = inicio;
         this.fim = fim;
+        this.idioma = idioma;
+        this.grau = grau;
+
     }
 
     public FilterProfessorObject(Map<String, String> parameters) {
@@ -29,18 +35,24 @@ public class FilterProfessorObject {
         String dia = parameters.get("dia");
         String inicio = parameters.get("inicio");
         String fim = parameters.get("fim");
+        String idioma = parameters.get("idioma");
+        String grau = parameters.get("grau");
 
-        if (curso == null && dia == null && inicio == null && fim == null)
+        if (curso == null && dia == null && inicio == null && fim == null && idioma == null && grau == null)
             logger.warn("Nenhum parametro inserido");
         else {
-            if (curso != null)
+            if (curso != null && !curso.equals("null"))
                 this.curso = Integer.parseInt(curso);
-            if (dia != null)
+            if (dia != null && !dia.equals("null"))
                 this.dia = DayOfWeek.of(Integer.parseInt(dia));
-            if (inicio != null)
+            if (inicio != null && !inicio.equals("null"))
                 this.inicio = LocalTime.parse(inicio);
-            if (fim != null)
+            if (fim != null && !fim.equals("null"))
                 this.fim = LocalTime.parse(fim);
+            if(idioma != null && !idioma.equals("null"))
+                this.idioma = idioma;
+            if (grau != null && !grau.equals("null"))
+                this.grau = Integer.parseInt(grau);
         }
 
     }
--- src/main/java/com/plataforma/explicacoes/services/filters/professor/ProfessorFilterByIdiomas.java
+++ src/main/java/com/plataforma/explicacoes/services/filters/professor/ProfessorFilterByIdiomas.java
@@ -1,31 +1,33 @@
 package com.plataforma.explicacoes.services.filters.professor;
 
 import com.plataforma.explicacoes.models.Horario;
+import com.plataforma.explicacoes.models.Idioma;
 import com.plataforma.explicacoes.models.Professor;
 
-import java.time.LocalTime;
+import javax.servlet.Filter;
 import java.util.HashSet;
 import java.util.Set;
 
-public class ProfessorFilterByHoraInicio implements FilterI<Professor>{
+public class ProfessorFilterByIdiomas implements FilterI<Professor> {
+    private String idioma;
 
-    private LocalTime inicio;
-
-    public ProfessorFilterByHoraInicio(LocalTime inicio){
-        this.inicio = inicio;
+    public ProfessorFilterByIdiomas(String idioma){
+        this.idioma = idioma;
     }
+
     @Override
     public Set<Professor> filter(Set<Professor> entities){
-        if(this.inicio == null)
+        if (this.idioma == null){
             return entities;
+        }
         Set<Professor> professors = new HashSet<>();
         for (Professor professor : entities){
-            for (Horario horario : professor.getHorarios()){
-                if (horario.getInicio().equals(inicio))
+            for (Idioma idioma : professor.getIdiomas()){
+                if(idioma.getName().equals(this.idioma)){
                     professors.add(professor);
+                }
             }
         }
         return professors;
     }
-
 }
